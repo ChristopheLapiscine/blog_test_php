058271ca054076fe39be0d525d4ff913ffe9c41b
Christophe Lorenzato <lorenzato.christophe@gmail.com>
2019-11-11 11:24:58 +0100

    système d'auth en place

--- src/Auth.php
+++ src/Auth.php
@@ -4,9 +4,16 @@
 namespace App;
 
 
+use App\Security\ForbiddenException;
+
 class Auth
 {
     public static function check (){
-        //TODO: Ecrire le code
+        if (session_status() === PHP_SESSION_NONE){
+            session_start();
+        }
+        if (!isset($_SESSION['auth'])){
+            throw new ForbiddenException();
+        }
     }
 }
--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace App;
 
+use App\Security\ForbiddenException;
+
 class Router {
 
     /**
@@ -44,16 +46,23 @@ class Router {
 
     public function run(){
         $match = $this->router->match();
-        $view = $match['target'];
+        $view = $match['target'] ?: '/e404';
         $params = $match['params'];
         $router = $this;
         $layout = $this;
         $isAdmin = strpos($view, 'admin/') !== false;
         $layout = $isAdmin ? 'admin/layouts/default' : 'layouts/default';
-        ob_start();
-        require $this->viewPath . $view . '.php';
-        $content = ob_get_clean();
-        require $this->viewPath . DIRECTORY_SEPARATOR . $layout . '.php';
+
+        try {
+            ob_start();
+            require $this->viewPath . $view . '.php';
+            $content = ob_get_clean();
+            require $this->viewPath . DIRECTORY_SEPARATOR . $layout . '.php';
+        } catch (ForbiddenException $e){
+            header('Location :' . $this->url('login') . '?forbidden=1');
+            exit();
+        }
+
 
         return $this;
     }
